e90d5ddb2816b4b6d5e7838a7b41b70f99947966
Kirill <ForStmKar@gmail.com>
2022-09-27 14:59:26 +0300

    telegram bot without token

--- Telegram-bot.py
+++ Telegram-bot.py
@@ -1,32 +1,46 @@
 import telebot
 from random import *
 import json
+import requests
 
 
-
-
-# def save():
-#     with open("films.json", 'w', encoding = 'utf-8') as fh:
-#         fh.write(json.dumps(films, ensure_ascii = False))
-#     print('Наша фильмотека была успешно сохранена в файле films.json')
 films = []
-        
-API_TOKEN ='5653912088:AAEdoH0JFcjdbhs6DZU6VkensUORyjw5pwg'
+
+API_URL = "https://7012.deeppavlov.ai/model"
+
+def save():
+    with open("films.json", 'w', encoding = 'utf-8') as fh:
+        fh.write(json.dumps(films, ensure_ascii = False))
+    print('Наша фильмотека была успешно сохранена в файле films.json')
+
+def load():
+    global films
+    with open("films.json", "r", encoding = 'utf-8') as fh:
+        films = json.load(fh)
+    print('Фильмотека была успешно загружена')
+
+
+API_TOKEN =''
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands = ['start'])
 def start_massage(message):
-    films.append('Матрица')
-    films.append('Солярис')
-    films.append('Хакеры')
-    films.append('Властелин колец')
-    films.append('Техасская резня бензопилой')
-    films.append('Санта барбара')
-    bot.send_message(message.chat.id, 'Привет, я заработал')
+    try:
+        load()
+        bot.send_message(message.chat.id, 'Фильмотека была успешно загружена')
+    except:
+        films.append('Матрица')
+        films.append('Солярис')
+        films.append('Хакеры')
+        films.append('Властелин колец')
+        films.append('Техасская резня бензопилой')
+        films.append('Санта барбара')
+        bot.send_message(message.chat.id, 'Фильмотека была загружена по умолчанию')
 
 @bot.message_handler(commands=['all'])
 def show_all(message):
-    bot.send_message(message.chat.id, 'Список фильмов: ', films)
+    bot.send_message(message.chat.id, 'Список фильмов:')
+    bot.send_message(message.chat.id, ', '.join(films))
 
 @bot.message_handler(commands=['help'])
 def show_help(message):
@@ -36,6 +50,16 @@ def show_help(message):
 def add_film(message):
     bot.send_message(message.chat.id, 'Название фильма, который вы хотите добавить: ')
 
+@bot.message_handler(commands=['wiki'])
+def wiki(message):
+    quest = message.text.split()[1:]
+    qq = ' '.join(quest)
+    data = {'question_raw': [qq]}
+    try:
+        res = requests.post(API_URL, json = data, verify = False).json()
+        bot.send_message(message.chat.id, res)
+    except:
+        bot.send_message(message.chat,id, 'Что-то я ничего не нашел')
 
 bot.polling()
 
