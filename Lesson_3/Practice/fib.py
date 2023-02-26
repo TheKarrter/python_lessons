3f0ce999269fec2823fa4a0dbb31b14726144c6d
Kirill <ForStmKar@gmail.com>
2023-02-26 11:32:19 +0300

    fib practice

---
+++ Lesson_3/Practice/fib.py
@@ -0,0 +1,11 @@
+def fib(n):
+    if n in {0,1}:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+for n in range(15):
+    if n < 14:
+        print(fib(n), end=', ')
+    else:
+        print(fib(n), end = '.')
